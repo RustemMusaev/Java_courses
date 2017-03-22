e45e8709eb813e1819043bfe2be9e1dfdfac420d
Мусаев Рустем <musaevrr@esm-group.local>
2017-03-22 20:00:00 +0700

    RT project commit

--- News/src/main/java/ru/rustem/controller/Controller.java
+++ News/src/main/java/ru/rustem/controller/Controller.java
@@ -1,28 +1,69 @@
 package ru.rustem.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import ru.rustem.model.*;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/news")
-    public ResponseEntity<String> getAllMessages(@RequestParam Map<String, String> param) {
-        Map<String, String> numericParameters = new HashMap<String, String>();
-        Map<String, String> stringParameters = new HashMap<String, String>();
+    @PostMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<String> returnXml(HttpServletRequest request) {
+        System.out.println(request.getRequestURI());
+        System.out.println(request.getParameterMap());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<ModelXml> returnXml(@RequestParam Map<String,String> param, HttpServletRequest request) {
+        List<XmlData> numericParameters = new LinkedList<>();
+        List<XmlData> stringParameters = new LinkedList<>();
+        System.out.println(request.getRequestURI());
 
         for (Map.Entry<String, String> entry : param.entrySet()) {
             if (valueIsStringParametrs(entry.getValue())) {
-                stringParameters.put(entry.getKey(), entry.getValue());
+                XmlData xmlDataString = new XmlData();
+                xmlDataString.setName(entry.getKey());
+                xmlDataString.setValue(entry.getValue());
+                stringParameters.add(xmlDataString);
             } else {
-                numericParameters.put(entry.getKey(), entry.getValue());
+                XmlData xmlDataNumeric = new XmlData();
+                xmlDataNumeric.setName(entry.getKey());
+                xmlDataNumeric.setValue(entry.getValue());
+                numericParameters.add(xmlDataNumeric);
             }
         }
 
-        return new ResponseEntity<String>(String.valueOf(param), HttpStatus.OK);
+        ModelXml modelXml = new ModelXml();
+        ClientInfo clientInfo = new ClientInfo();
+        String browserName = request.getHeader("User-Agent");
+        String ipAdd = request.getRemoteAddr();
+        clientInfo.setIpAddress(ipAdd);
+        clientInfo.setUserAgent(browserName);
+        modelXml.setClientInfo(clientInfo);
+
+        Parametrs parametrs = new Parametrs();
+
+        ResData resDataNumeric = new ResData();
+        resDataNumeric.setData(numericParameters);
+        parametrs.setNumericHashMap(resDataNumeric);
+
+        ResData resDataString = new ResData();
+        resDataString.setData(stringParameters);
+        parametrs.setStringHashMap(resDataString);
+
+        parametrs.setRequestMethod(request.getMethod());
+
+        modelXml.setParametrs(parametrs);
+        return new ResponseEntity<>(modelXml, HttpStatus.OK);
     }
 
     public boolean valueIsStringParametrs(String value) {
